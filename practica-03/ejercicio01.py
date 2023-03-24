9bb57ef35e6c533ac34362d0ed04c86d6e6fce0c
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-03-24 17:14:47 -0300

    uploading practica-03

---
+++ practica-03/ejercicio01.py
@@ -0,0 +1,44 @@
+"""Constructor, Variables de instancia y métodos de instacia"""
+
+from typing import Optional
+
+
+class Rectangulo:
+    """
+    Implementar la clase Rectangulo que contiene una base y una altura, y el
+    método area.
+    """
+
+    def __init__(self, base: Optional[int] = None, altura: Optional[int] = None):
+        self.base = base
+        self.altura = altura
+
+    def area(self) -> int:
+        return self.base * self.altura
+
+
+# NO MODIFICAR - INICIO
+
+# Test Constructor
+rec = Rectangulo(10, 10)
+assert rec.base == 10
+assert rec.altura == 10
+assert rec.area() == 100
+
+# Test Valores por defecto
+rec = Rectangulo()
+assert rec.base is None
+assert rec.altura is None
+assert rec.area() == 0
+
+rec.base = 10
+rec.altura = 10
+assert rec.base == 10
+assert rec.altura == 10
+assert rec.area() == 100
+
+# Test Instanciación sin variable
+assert Rectangulo(10, 10).area() == 100
+assert Rectangulo(10, 0).area() == 0
+assert Rectangulo(0, 10).area() == 0
+# NO MODIFICAR - FIN
