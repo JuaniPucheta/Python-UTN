a9b6ad891e015ae988df27486dd0f9cb984edc97
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-05-07 18:27:40 -0300

    doing some kivy

--- kivy/main.py
+++ kivy/main.py
@@ -2,7 +2,49 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivy.properties import BooleanProperty
 
+class WidgetsExample(GridLayout):
+    count = 1
+    count_enabled = BooleanProperty(False)
+    my_text = StringProperty("1")
+
+    def on_button_click(self):
+        print("Button clicked")
+        if self.count_enabled:
+            self.count += 1
+            self.my_text = str(self.count)
+
+    def on_toggle_button_state(self, widget):
+        print("Toggle state: " + widget.state)
+        if widget.state == "normal":
+            widget.text = "OFF"
+            self.count_enabled = False
+        else:
+            widget.text = "ON"
+            self.count_enabled = True
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "rl-bt"
+        for i in range(0, 100):
+            #size = dp(100) + i * 10
+            size = dp(100)
+            b = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
+            self.add_widget(b)
+
+# class GridLayoutExample(GridLayout):
+#     pass
+#?      en el archivo theLab.kv, se llamada de otra forma a la clase GridLayoutExample
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
 
 class BoxLayoutExample(BoxLayout):
     pass
